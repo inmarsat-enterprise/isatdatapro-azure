a428856485fe67cd27cd8287131324fb273253b6
Geoff Bruce-Payne <gbrucepayne@hotmail.com>
2021-03-07 08:06:05 -0500

    Update idpdevkit message parser

--- device-bridge/lib/deviceModels/inmarsatPnpDevKit/messageParser.js
+++ device-bridge/lib/deviceModels/inmarsatPnpDevKit/messageParser.js
@@ -34,25 +34,69 @@ function writeProperty(propName, propValue) {
   }
   let otaMessage = {};
   switch (propName) {
-    case 'locationInterval':
-      //: An example using payloadRaw
-      if (propValue < 0 || propValue > 1440) {
-        throw new Error(`locationInterval must be in range [0..1440]`);
-      }
-      const intervalBytes = [
-        (propValue & 0xff00) >> 8,
-        (propValue & 0x00ff)
-      ];
+    case 'reportGet':
       otaMessage.command = {
-        payloadRaw: ([255, 255]).concat(intervalBytes)
+        payloadJson: {
+          codecServiceId: 255,
+          codecMessageId: 1,
+          fields: []
+        }
+      };
+    case 'configSet':
+      otaMessage.command = {
+        payloadJson: {
+          codecServiceId: 255,
+          codecMessageId: 2,
+          fields: []
+        }
+      };  
+      if (propValue.reportInterval) {
+        if (propValue.reportInterval < 0 || propValue.reportInterval > 86400) {
+          throw new Error(`reportInterval must be in range [0..86400]`);
+        }
+        otaMessage.command.payloadJson.fields.push({
+          name: 'reportInterval',
+          stringValue: `${propValue.reportInterval}`
+        });
+      }
+      if (propValue.qosInterval) {
+        if (propValue.qosInterval < 0 || propValue.qosInterval > 86400) {
+          throw new Error(`qosInterval must be in range [0..86400]`);
+        }
+        otaMessage.command.payloadJson.fields.push({
+          name: 'qosInterval',
+          stringValue: `${propValue.qosInterval}`
+        });
+      }
+      break;
+    case 'textMobileTerminated':
+      otaMessage.command = {
+        payloadJson: {
+          codecServiceId: 255,
+          codecMessageId: 3,
+          fields: [
+            {
+              name: 'text',
+              stringValue: propValue
+            }
+          ]
+        }
+      };
+    case 'configGet':
+      otaMessage.command = {
+        payloadJson: {
+          codecServiceId: 255,
+          codecMessageId: 4,
+          fields: []
+        }
       };
   }
   //TODO: add additional writable properties / commands here
+  return otaMessage;
 }
 
 function parsePnpDevkit(context, message) {
   //context.log.verbose(`Parsing PNP Devkit fields: ${JSON.stringify(message)}`);
-  const codecMessageId = message.payloadJson.codecMessageId;
   const messageIsJson = (message.payloadJson && message.payloadJson !== null)
   let {telemetry, reportedProperties, timestamp } =
       idpDefault.parse(context, message);
@@ -60,8 +104,8 @@ function parsePnpDevkit(context, message) {
     context.log.warning(`Message definition file missing`
         + ` for Mailbox ID ${message.mailboxId}`);
   } else if (message.codecServiceId === 255) {
-    switch(codecMessageId) {
-      case 255:
+    switch(message.payloadJson.codecMessageId) {
+      case 1:
         telemetry.latitude = round(telemetry.latitude/60000, 6);
         telemetry.longitude = round(telemetry.longitude/60000, 6);
         telemetry.snr = round(telemetry.snr / 10, 1);
@@ -71,6 +115,18 @@ function parsePnpDevkit(context, message) {
           "alt": telemetry.altitude
         };
         break;
+      case 2:
+      case 4:
+        if (telemetry.reportInterval) {
+          reportedProperties.reportInterval = telemetry.reportInterval;
+        }
+        if (telemetry.qosInterval) {
+          reportedProperties.qosInterval = telemetry.qosInterval;
+        }
+        break;
+      // Other cases pass through from idpDefault parser
+      case 3:
+        reportedProperties.textMobileOriginated = telemetry.text;
     }
   }
   return { telemetry, reportedProperties, timestamp };
